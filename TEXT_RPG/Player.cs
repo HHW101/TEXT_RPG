3bd84380a7e04d27126b5e51c80b962f6c04a318
KGH1125 <128346998+KGH1125@users.noreply.github.com>
2025-04-25 15:18:28 +0900

    정렬

--- TEXT_RPG/Player.cs
+++ TEXT_RPG/Player.cs
@@ -8,42 +8,26 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace TEXT_RPG
-{
-    internal class Player :Unit
+{   //공격 처리를 위하여 Unit 상속
+    internal class Player : Unit
     {
-     
-        //공격 처리를 위하여 Unit에게 상속 받게 함.
-
         public Job Job { get; set; } // 플레이어 직업
-        //private static Player instance;
-        //public static Player Instance()
-        //{
-        //    if (instance == null)
-        //        instance = new Player();
-        //    return instance;
-        //}
+
         public Player()
         {
             equippedItems = ItemManager.Instance().items.Where(x => x.IsEquipped).ToList();
-         
         }
 
         public float TotalAttack => ATK + CalculateEquippedBonuses().Attack; //최종 공격력
         public float TotalDefense => DEF + CalculateEquippedBonuses().Defense; //최종 방어력
-
         public int TotalMaxHP => MaxHp + CalculateEquippedBonuses().MaxHP; //최종 최대 체력
-
         public int TotalMaxMP => MaxMp + CalculateEquippedBonuses().MaxMP; //최종 최대 마나
-
-
-       
-
 
         public bool IsAlive => CurrentHP > 0; //살아있나 확인...
 
         public List<Item> equippedItems = ItemManager.Instance().items.Where(x => x.IsEquipped).ToList();//착용한 아이템 리스트
         public List<Item> inventory = ItemManager.Instance().items.Where(x => x.IsHave).ToList(); // 인벤토리 아이템 리스트
-      
+
         //public void SetJob(Dictionary<string, object> data)
         //{
         //    Job = (Job)Enum.Parse(typeof(Job), (string)data["Name"]);
@@ -63,7 +47,6 @@ namespace TEXT_RPG
         //}
         public void SetJob(Job job)
         {
-
             this.Job = job;
             ATK += Job.Attack;
             DEF += Job.Defense;
@@ -73,13 +56,12 @@ namespace TEXT_RPG
             CurrentMP = MaxMp;
             Speed += Job.speed;
 
-
             skills.AddRange(job.SkillList);
         }
         public void ShowStat() //플레이어 스텟 보여주기
         {   //여기서 부터
             BonusStat bonus = CalculateEquippedBonuses();
-            
+
             Console.WriteLine($"Lv.{Level}");
             Console.WriteLine($"{Name} ( {Job.ToString()} )");
             Console.WriteLine($"공격력 : {TotalAttack} (+{bonus.Attack})");
@@ -89,13 +71,13 @@ namespace TEXT_RPG
             Console.WriteLine($"회피율 : {Evasion} (+{bonus.Evasion})");
             Console.WriteLine($"치명타율 : {Critical} (+{bonus.Critical})");
             Console.WriteLine($"Gold : {Gold} G");
-            Thread.Sleep( 1000 );
+            Thread.Sleep(1000);
         }
         public BonusStat CalculateEquippedBonuses()//플레이어 장착 아이템 능력치 총합 계산
         {
             equippedItems = ItemManager.Instance().items.Where(x => x.IsEquipped).ToList();
             BonusStat bonus = new();
-            
+
             foreach (var item in equippedItems)
             {
                 switch (item.MainType)
@@ -118,21 +100,23 @@ namespace TEXT_RPG
                         break;
                 }
             }
-
             return bonus;
         }
+
         public List<Item> ShowEInvenItems()
         {
             equippedItems = ItemManager.Instance().items.Where(x => x.IsEquipped).ToList();
-            
             return equippedItems;
         }
+
         public List<Item> ShowInvenItems()
         {
             inventory = ItemManager.Instance().items.Where(x => x.IsHave).ToList();
             return inventory;
         }
+
         public void ShowInventory() { }
+
         public void ShowSkillList() //플레이어 스킬 목록 표시
         {
             Console.WriteLine($"[{Name}]의 스킬 목록:");
@@ -142,22 +126,24 @@ namespace TEXT_RPG
                 Console.WriteLine($"{i + 1}. {skill.Name} - {skill.Description} (MP: {skill.MPCost})");
             }
         }
+
         public string ShowSkillListS() //씬에서의 플레이어 스킬 목록 표시
         {
             string x = "";
             for (int i = 0; i < skills.Count; i++)
             {
                 Skill skill = skills[i];
-                x+=($"{i + 1}. {skill.Name} - {skill.Description} (MP: {skill.MPCost})\n\n");
+                x += ($"{i + 1}. {skill.Name} - {skill.Description} (MP: {skill.MPCost})\n\n");
             }
             return x;
         }
+
         public string showInfo()
         {
             BonusStat bonus = CalculateEquippedBonuses();
             string str = "\n\n";
-            str+=($"Lv.{Level}\n");
-            str+=($"{Name} ( {Job.Name} )\n");
+            str += ($"Lv.{Level}\n");
+            str += ($"{Name} ( {Job.Name} )\n");
             str += ($"공격력 : {TotalAttack} (+{bonus.Attack})\n");
             str += ($"방어력 : {TotalDefense} (+{bonus.Defense})\n");
             str += ($"HP : {CurrentHP} / {TotalMaxHP} (+{bonus.MaxHP})\n");
@@ -167,6 +153,7 @@ namespace TEXT_RPG
             str += ($"Gold : {Gold} G");
             return str;
         }
+
         public void LevelUp() //플레이어 레벨업
         {
             Level++;
@@ -177,15 +164,13 @@ namespace TEXT_RPG
             CurrentHP = TotalMaxHP;
             CurrentMP = TotalMaxMP;
             //레벨업시 추가 요소 여기다 작성
-
         }
-     
+
         protected override void Dead()//플레이어 죽음
         {
             //마을(타이틀)로 돌아감
             //패널티가 있다면 여기에
             base.Dead();
-
         }
 
         public Skill UseSkill()//스킬 선택 후 사용
@@ -237,5 +222,4 @@ namespace TEXT_RPG
             }
         }
     }
-
 }
